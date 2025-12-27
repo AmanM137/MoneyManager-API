65ed2ff41f3b4a550a813c480bd4d5369b3d5606
Aman Kumar Singh <aman20012004@gmail.com>
2025-12-27 22:28:36 +0530

    Changed the local frontend url to deployed frontend url

--- MoneyManager-API/src/main/java/in/finance/moneymanager/config/SecurityConfig.java
+++ MoneyManager-API/src/main/java/in/finance/moneymanager/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
                 //"http://localhost:5173"
-                "https://69500c98e05f9208ccd1afd2--moneymanager20.netlify.app" // your deployed frontend domain
+                "https://moneymanager20.netlify.app/login" // your deployed frontend domain
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Accept", "Content-Type"));
